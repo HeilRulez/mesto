4ad0636115c8b6afbc1ad4a524ff0bf8d1fd47b1
HeilRulez <Rulez.Heil@gmail.com>
2022-04-03 03:00:46 +0600

    длбить добавление слушателей

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const overlayForProfile = document.querySelector('.overlay_for_profile'),
   profileInfoBtn = document.querySelector('.profile-info__btn'),
-  modalFormCloseForProfile = document.querySelector('.modal-form__close_for_profile'),
+  modalFormClose = document.querySelectorAll('.modal-form__close'),
   formForProfile = document.querySelector('.form_for_profile'),
   formNameForProfile = document.querySelector('.form__name_for_profile'),
   formDataForProfile = document.querySelector('.form__data_for_profile'),
@@ -9,7 +9,6 @@ const overlayForProfile = document.querySelector('.overlay_for_profile'),
 
 const overlayForAddCard = document.querySelector('.overlay_for_addCard'),
   profileAddBtn = document.querySelector('.profile__add-btn'),
-  modalFormCloseForAddCard = document.querySelector('.modal-form__close_for_addCard'),
   formForAddCard = document.querySelector('.form_for_addCard'),
   formNameForAddCard = document.querySelector('.form__name_for_addCard'),
   formDataForAddCard = document.querySelector('.form__data_for_addCard'),
@@ -18,19 +17,23 @@ const overlayForAddCard = document.querySelector('.overlay_for_addCard'),
 
 const overlayForView = document.querySelector('.overlay_for_view'),
   modalFormViewImg = document.querySelector('.modal-form__view-img'),
-  modalFormCloseForView = document.querySelector('.modal-form__close_for_view'),
   modalFormTitleForView = document.querySelector('.modal-form__title_for_view');
 
 // Закрытие любого модального окна
-function closeForm(evt) {
-  evt.target.closest('.overlay').classList.remove('overlay_visible_on');
+function closeForm(modal) {
+  modal.classList.remove('overlay_visible');
+}
+
+// ОТкрытие любого модального окна
+function openModal(modal) {
+  modal.classList.add('overlay_visible');
 }
 
 // Форма данных профиля
 function openEditProfile(evt) {
+  openModal(overlayForProfile);
   formNameForProfile.value = profileInfoName.textContent;
   formDataForProfile.value = profileInfoDiscription.textContent;
-  overlayForProfile.classList.add('overlay_visible_on');
 }
 
 // Отправка данных профиля из формы на страницу
@@ -43,20 +46,25 @@ function saveData(evt) {
 
 // Форма добавления контента
 function openAddCard() {
-  overlayForAddCard.classList.add('overlay_visible_on');
-  formNameForAddCard.value = '';
-  formDataForAddCard.value = '';
+  openModal(overlayForAddCard);
+  formForAddCard.reset();
 }
 
 // Создание карточки контента из шаблона
-function addCard(name='', data) {
-  cardItem = sampleCard.querySelector('.card').cloneNode(true);
-  cardItem.querySelector('.card__title').textContent = name;
-  cardItem.querySelector('.card__img').src = data;
-  cardItem.querySelector('.card__img').alt = name;
-  cardItem.querySelector('.card__del').addEventListener('click', delCard, false);
-  cardItem.querySelector('.card__like').addEventListener('click', likeCard, false);
-  cardItem.querySelector('.card__img').addEventListener('click', scaleImg, false);
+function createCard(name = '', data) {
+  let cardCopy = sampleCard.querySelector('.card').cloneNode(true);
+  cardCopy.querySelector('.card__title').textContent = name;
+  cardCopy.querySelector('.card__img').src = data;
+  cardCopy.querySelector('.card__img').alt = name;
+  cardCopy.querySelector('.card__del').addEventListener('click', delCard, false);
+  cardCopy.querySelector('.card__like').addEventListener('click', likeCard, false);
+  cardCopy.querySelector('.card__img').addEventListener('click', scaleImg, false);
+  return cardCopy;
+}
+
+// Добавление карточки в дом
+function addCard(name, data) {
+  let cardItem = createCard(name, data);
   cards.prepend(cardItem);
 }
 
@@ -64,10 +72,18 @@ function addCard(name='', data) {
 function btnAddCard(evt) {
   evt.preventDefault();
   let name = formNameForAddCard.value,
-      data = formDataForAddCard.value;
+    data = formDataForAddCard.value;
   addCard(name, data);
-  cardsData.push({name: name, link: data});
+  addDataInbase(cardsData, name, data);
   closeForm(evt);
+}
+
+// Тут дожна бфть отправка на сервер
+function addDataInbase(item, name, data) {
+  item.push({
+    name: name,
+    link: data
+  });
 }
 
 function delCard(evt) {
@@ -80,7 +96,7 @@ function likeCard(evt) {
 
 // Увеличение картинки
 function scaleImg(evt) {
-  overlayForView.classList.add('overlay_visible_on');
+  openModal(overlayForView);
   modalFormViewImg.src = evt.target.src;
   modalFormViewImg.alt = evt.target.alt;
   modalFormTitleForView.textContent = evt.target.alt;
@@ -88,14 +104,19 @@ function scaleImg(evt) {
 
 profileInfoBtn.addEventListener('click', openEditProfile, false); //данные профиля
 formForProfile.addEventListener('submit', saveData, false); // отправка данных профиля
-profileAddBtn.addEventListener('click', openAddCard, false);  // выбор контента
-formForAddCard.addEventListener('submit', btnAddCard, false);  // добавить контент на страницу
-modalFormCloseForProfile.addEventListener('click', closeForm, false);
-modalFormCloseForAddCard.addEventListener('click', closeForm, false);
-modalFormCloseForView.addEventListener('click', closeForm, false);
+profileAddBtn.addEventListener('click', openAddCard, false); // выбор контента
+formForAddCard.addEventListener('submit', btnAddCard, false); // добавить контент на страницу
 
-const cardsData = [
-  {
+modalFormClose.forEach((modal) => {
+  modal.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('modal-form__close')) {
+      let overlay = evt.target.closest('overlay');
+      closeForm(overlay);
+    };
+  });
+});
+
+const cardsData = [{
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
